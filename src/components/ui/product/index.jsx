050fce900f8adeb5cd58d5140846c5b81590e0f7
iamtun <letuan19431791.iuh@gmail.com>
2024-07-28 14:53:41 +0700

    feat: add sale with condition for product

--- src/components/ui/product/index.jsx
+++ src/components/ui/product/index.jsx
@@ -7,6 +7,9 @@ const Product = ({ product }) => {
     <Card title={product.title} className={styles.container}>
       <img src={product.image} />
       <p className={styles.price}>${product.price}</p>
+      <p className={styles.description}>{product.short_description}</p>
+
+      {product.is_sale ? <div className={styles.sale}>SALE</div> : null}
 
       <Button className={styles.btn}>Add to card</Button>
     </Card>
