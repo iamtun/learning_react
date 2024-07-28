1e86f8824127400b11a06575b02cf12c38dfc5bd
iamtun <letuan19431791.iuh@gmail.com>
2024-07-27 20:23:10 -0400

    update base url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/iamtun/learning_react'
+  base: '/learning_react'
 })
