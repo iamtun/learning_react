b38f8db3ad3174211b87e5b4ca8b89294327377e
iamtun <letuan19431791.iuh@gmail.com>
2024-07-28 12:36:27 +0700

    feat: add router for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { AppLayout } from "./components/layouts";
+import { CardPage, HomePage, ProductPage } from "./views";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+      {
+        path: "/products",
+        element: <ProductPage />,
+      },
+      {
+        path: "/card",
+        element: <CardPage />,
+      },
+    ],
+  },
+]);
+
 const App = () => {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
