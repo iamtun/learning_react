0b4f8ec71b0caec3a13a484b1067cf87fc9fde97
iamtun <letuan19431791.iuh@gmail.com>
2024-07-28 15:24:59 +0700

    feat: add store to root app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { AppLayout } from "./components/layouts";
 import { CardPage, HomePage, ProductPage } from "./views";
+import { Provider } from "react-redux";
+import store from "@/store";
 
 const router = createBrowserRouter([
   {
@@ -24,7 +26,11 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
 };
 
 export default App;
