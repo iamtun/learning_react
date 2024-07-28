015c86074584e7a7c13949e677cdf3fe35e6ffb2
iamtun <letuan19431791.iuh@gmail.com>
2024-07-28 12:34:50 +0700

    feat: add config for alias path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/'
-})
+export default defineConfig(() => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  return {
+    plugins: [react()],
+    base: "/",
+    resolve: {
+      alias: [
+        {
+          find: "@",
+          replacement: path.resolve(__dirname, "./src"),
+        },
+      ],
+    },
+  };
+});
